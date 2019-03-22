bf73956eb94ecf7d4f842122f9e7cd0bb75140c5
ColeyG <cjgeerts@gmail.com>
2019-03-21 20:48:23 -0400

    engine running and configuration

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Items On
 
-An electron game made with [excalibur.js](https://excaliburjs.com/)
+An [electron](https://electronjs.org/docs) game made with [excalibur.js](https://excaliburjs.com/).
+
+This is a project made with a love of making projects 💙.
--- renderer.js
+++ renderer.js
@@ -1,10 +1,6 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
 const fs = require('fs');
 
 let outFromFile = fs.readFileSync('hello.txt', 'utf8');
 
-document.querySelector('.test').style.backgroundColor = 'blue';
-
+console.log(ex);
 console.log(outFromFile);
